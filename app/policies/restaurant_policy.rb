9e458e245b41cb145206480636d2965eb83652ee
Roberto Barros <roberto.f.barros@gmail.com>
2020-11-24 10:26:31 -0300

    livecode

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,25 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user || user.admin?
+  end
+
+
+end
